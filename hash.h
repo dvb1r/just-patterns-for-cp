612ffb8597eec16e992cb4846e884c8ad000d366
dvb1r <135020288+dvb1r@users.noreply.github.com>
2025-05-19 20:42:30 +0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -7,9 +7,6 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-const int mod = 1e9 + 7;
-
-#include "ModularInt.h"
 
 template<typename t>
 void init(vector<Mint>& pow, vector<ull>& pow2, vector<Mint>& pref, vector<ull>& pref2, t s, bool rev = false) {
@@ -43,5 +40,20 @@ t gethash(int l, int r, const vector<t>& pow, const vector<t>& pref, bool rev = 
     }
     return (t)(pref[r] - (l < 1 ? 0 : pref[l - 1] * pow[r - l + 1]));
 }
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
 
 #endif //HASH_H
